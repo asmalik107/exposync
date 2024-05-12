e2b6a47d9c3899c4f5fe7b95c7909c42304be022
asmalik107 <asmalik@outlook.com>
2024-05-12 12:18:28 +0100

    chore: add zustand

--- src/app/(app)/(tabs)/index.tsx
+++ src/app/(app)/(tabs)/index.tsx
@@ -1,5 +1,18 @@
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { MMKV } from 'react-native-mmkv';
+import { create } from 'zustand';
+
+interface TestStore {
+  count: number;
+  increaseCount: () => void;
+  removeAll: () => void;
+}
+
+const useTestStore = create<TestStore>((set) => ({
+  count: 0,
+  increaseCount: () => set((state) => ({ count: state.count + 1 })),
+  removeAll: () => set({ count: 0 }),
+}));
 
 export const storage = new MMKV();
 storage.set('user.name', 'Marc');
@@ -7,6 +20,9 @@ storage.set('user.age', 21);
 storage.set('is-mmkv-fast-asf', true);
 
 export default function Index() {
+  const count = useTestStore((state) => state.count);
+  const increaseCount = useTestStore((state) => state.increaseCount);
+
   const username = storage.getString('user.name'); // 'Marc'
   const age = storage.getNumber('user.age'); // 21
   const isMmkvFastAsf = storage.getBoolean('is-mmkv-fast-asf'); // true
@@ -17,6 +33,9 @@ export default function Index() {
       <Text>{username}</Text>
       <Text>{age}</Text>
       <Text>{isMmkvFastAsf}</Text>
+      <Pressable onPress={increaseCount}>
+        <Text>Count: {count}</Text>
+      </Pressable>
     </View>
   );
 }
