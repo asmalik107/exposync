36886d31743d04b199e29e98e5a800e9710e49c9
asmalik107 <asmalik@outlook.com>
2024-05-12 11:43:02 +0100

    feat: move sign out to settings

--- src/app/(app)/(tabs)/index.tsx
+++ src/app/(app)/(tabs)/index.tsx
@@ -1,16 +1,9 @@
 import { Text, View } from 'react-native';
-import { useSession } from 'src/providers/auth-context';
 
 export default function Index() {
-  const { signOut } = useSession();
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text
-        onPress={() => {
-          signOut();
-        }}>
-        Sign Out
-      </Text>
+      <Text>Home</Text>
     </View>
   );
 }
--- src/app/(app)/(tabs)/settings.tsx
+++ src/app/(app)/(tabs)/settings.tsx
@@ -1,9 +1,17 @@
 import { View, Text } from 'react-native';
+import { useSession } from 'src/providers/auth-context';
 
 export default function Tab() {
+  const { signOut } = useSession();
+
   return (
     <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <Text>Settings</Text>
+      <Text
+        onPress={() => {
+          signOut();
+        }}>
+        Sign Out
+      </Text>
     </View>
   );
 }
